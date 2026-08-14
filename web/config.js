f5344f7de651473191504e8bfabe650d767af5d8
digihori <yo6987@gmail.com>
2026-08-14 12:18:39 +0900

    Configure production Web OAuth client

--- web/config.js
+++ web/config.js
@@ -1,5 +1,5 @@
 // 本番Web OAuthクライアントの発行後、Client IDを設定する。
 // Client IDは公開情報だが、Client Secretは絶対に配置しない。
 window.SOLIMEMO_CONFIG = {
-  googleClientId: "",
+  googleClientId: "1022038692225-t2ad5kvmghr635o72i8r58ocid2h7113.apps.googleusercontent.com",
 };
